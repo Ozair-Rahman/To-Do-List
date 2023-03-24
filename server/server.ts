25b662118d2a8a42b646ed97eccc18a8517cc520
Ozair-Rahman <ozairrahman2003@gmail.com>
2023-03-24 15:27:49 -0400

    Created client & server dir & started base API Request

---
+++ server/server.ts
@@ -0,0 +1,11 @@
+// Import Dependencies
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get('/', (req:any, res:any) => res.send("Hello World!"));
+
+app.listen(PORT, () => console.log(`Running on Port: ${PORT}`));
